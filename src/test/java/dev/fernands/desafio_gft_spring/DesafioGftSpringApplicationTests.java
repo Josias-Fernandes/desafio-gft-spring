88b390e57387dc9fda960d4c2be8f4a5b7354c10
Josias-Fernandes <josiasfernands.work@outlook.com>
2024-07-11 23:13:04 -0300

    Projeto iniciado

---
+++ src/test/java/dev/fernands/desafio_gft_spring/DesafioGftSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fernands.desafio_gft_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioGftSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
